ba93c1c1863e766e90775b36ea98004ef274fd29
abar.toha <abar.toha@gmail.com>
2024-11-27 11:38:12 +0600

    second proper run

--- scraper.py
+++ scraper.py
@@ -15,7 +15,7 @@ soup = BeautifulSoup(html_content, 'html.parser')
 mother_div = soup.find('div', attrs={"property": "text", "class": "uk-margin-medium-top"})
 
 # Find the lists for all the games
-game_data = [{x.text: x['href']} for ul in mother_div.find_all('ul') for x in ul.find_all('a')]
+game_data = {x.text: x['href'] for ul in mother_div.find_all('ul') for x in ul.find_all('a')}
 
 # Save the extracted list as a JSON file
 with open("games.json", "w", encoding='utf-8') as outfile:
